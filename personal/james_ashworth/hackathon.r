86443f0fc20d474d13e50be33f7897d7fa43b4e6
James Ashworth <james.b.ashworth@gmail.com>
2021-10-10 19:59:34 -0400

    updating with census data.

--- personal/james_ashworth/hackathon.r
+++ personal/james_ashworth/hackathon.r
@@ -54,3 +54,31 @@ write.csv('C:/code/p3_AshLee/data/v1_base_b4_census.csv', x = datNest2)
 
 # If need to read in material after blowing up pc again
 datNest2 <- read_csv("C:/code/p3_AshLee/data/v1_base_b4_census.csv")
+def2 <- read_csv("C:/code/p3_AshLee/hackathon_data/safegraph_open_census_data_2019/data/cbg_b03.csv")
+def4 <- read_csv("C:/code/p3_AshLee/hackathon_data/safegraph_open_census_data_2019/data/cbg_b01.csv")
+def5 <- read_csv("C:/code/p3_AshLee/hackathon_data/safegraph_open_census_data_2019/data/cbg_b19.csv")
+
+# CReate df with unique data
+# made no change
+#datNest2_stripped = subset(datNest2, select = -c(placekey, brands, city, visitor_cbg))
+
+# Create other race column
+def2 <- def2 %>%
+    mutate(other = (B03002e5+B03002e7+B03002e8+B03002e9))
+
+# create subset of just race counts
+def3 <- def2 %>%
+    select(census_block_group, B03002e1, B03002e3, B03002e4, B03002e6, B03002e12, other)
+
+def4 = subset(def4, select = c(census_block_group,B01002e1))
+def5 = subset(def5, select = c(census_block_group,B19013e1))
+
+# merge race data with main data
+datNest3 <- merge(datNest2, def3, by.x = c('name'), by.y = c('census_block_group'))
+datNest3 <- merge(datNest3, def4, by.x = c('name'), by.y = c('census_block_group'))
+datNest3 <- merge(datNest3, def5, by.x = c('name'), by.y = c('census_block_group'))
+
+head(datNest3)
+
+# Write data with census data metrics
+write.csv('C:/code/p3_AshLee/data/v1_base_with_census_metrics.csv', x = datNest3)
