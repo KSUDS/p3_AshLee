7925af008d5b26176e08d5d563e41ad90b8ad427
James Ashworth <james.b.ashworth@gmail.com>
2021-10-10 19:59:49 -0400

    updaing wiht census data

--- personal/james_ashworth/hackathon.py
+++ personal/james_ashworth/hackathon.py
@@ -7,10 +7,11 @@ from plotnine import theme
 from plotnine import *
 # %%
 # Read my dataset from exported R code
-gm = pd.read_csv("C:/code/p3_AshLee/data/.csv")
-
-
-
+datNest2 = pd.read_csv("C:/code/p3_AshLee/data/v1_base_b4_census.csv")
+datNest2 = datNest2.drop('visitor_cbg', 1)
+datNest2['poi_cbg'] = datNest2.poi_cbg.astype(str)
+datNest2['poi_cbg'] = datNest2['poi_cbg'].str[:12]
+datNest2['poi_cbg'] = datNest2.poi_cbg.astype(object)
 
 # %%
 # bring in data from R vertical breakage
