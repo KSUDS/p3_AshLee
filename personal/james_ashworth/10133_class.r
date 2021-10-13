f64906eda4db65977e7eaad2090c35768a2190e9
James Ashworth <james.b.ashworth@gmail.com>
2021-10-13 16:48:16 -0400

    class for 1013

--- personal/james_ashworth/10133_class.r
+++ personal/james_ashworth/10133_class.r
@@ -68,3 +68,63 @@ calw %>%
     theme_bw() +
     theme(legend.position = "bottom") +
     labs(fill = "Number of Chipotle\nstores")
+
+
+######   Another example
+
+dat_wc <- st_join(dat, cal, join = st_within) 
+
+# unnest
+
+days_week_long <- dat_wc %>%
+    filter(region == "CA") %>%
+    as_tibble() %>% # notice this line to break the sf object rules.
+    rename(name_county = name) %>%
+    unnest(popularity_by_day) %>%
+    select(placekey, city, region, contains("raw"),
+        name, value, geometry, countyfp, name_county)
+
+
+days_week <-  days_week_long %>%
+    pivot_wider(names_from = name, values_from = value)
+
+visits_day_join <- days_week %>%
+    group_by(countyfp, name_county) %>%
+    summarise(
+        count = n(),
+        Monday = sum(Monday, na.rm = TRUE) / count,
+        Tuesday = sum(Tuesday, na.rm = TRUE) / count,
+        Wednesday = sum(Wednesday, na.rm = TRUE) / count,
+        Thursday = sum(Thursday, na.rm = TRUE) / count,
+        Friday = sum(Friday, na.rm = TRUE) / count,
+        Saturday = sum(Saturday, na.rm = TRUE) / count,
+        Sunday = sum(Sunday, na.rm = TRUE) / count,
+    ) %>%
+    ungroup()
+
+calw <- calw %>%
+    left_join(visits_day_join %>% select(-name_county)) %>%
+    replace_na(list(Monday = 0, Tuesday = 0, Wednesday = 0,
+      Thursday = 0, Friday = 0, Saturday = 0, Sunday = 0)) 
+
+calw %>%
+ggplot() +
+    geom_sf(aes(fill = Saturday)) + 
+    geom_sf(data = filter(dat, region == "CA"), color = "white", shape = "x") +
+    theme_bw() +
+    theme(legend.position = "bottom") +
+    labs(fill = "Average store traffic", 
+      title = "Saturday traffic for Chipotle")      
+
+
+calw %>%
+ggplot() +
+    geom_sf(aes(fill = Saturday)) + 
+    geom_sf(aes(geometry = sf_center, size = count), color = "grey") +
+    theme_bw() +
+    scale_size_continuous(breaks = c(1, 5, 10, 25, 50, 75),
+      trans = "sqrt", range = c(2, 15)) +
+    labs(
+        fill = "Average store traffic",
+        size = "Number of stores",
+        title = "Saturday traffic for Chipotle")
