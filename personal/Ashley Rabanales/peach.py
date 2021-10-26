f3a59dd1c84b09c2b26047f4545d7935161cd307
ashleyrabanales <arabana2@students.kennesaw.edu>
2021-10-26 11:16:30 -0400

    10/26

--- personal/Ashley Rabanales/peach.py
+++ personal/Ashley Rabanales/peach.py
@@ -3,9 +3,19 @@ import altair as alt
 import numpy as py
 import plotnine as plt
 import matplotlib as mat
-import geompandas as geom 
+import geopandas as gpd
+
+import sfarrow as sf
+
+import sys
+!{sys.executable} -m pip install geopandas
+
+#ga <- USAboundaries::us_counties(states = 'Georgia')
+
 #%% 
-datNest4 = pd.read_csv("C:\Users\ashleyrabanales\Projects_ST\p3_AshLee\data\v1_base_with_census_metrics.csv")
-print (datNest4)
+datNest4 = pd.read_csv("/Users/ashleyrabanales/Projects_ST/p3_AshLee/data/v1_base_with_census_metrics.csv")
+print (datNest4) #tell if import correctly
+
+ga = pd.read_csv("/Users/ashleyrabanales/Projects_ST/p3_AshLee/data/ga.csv")
 
 #%%
